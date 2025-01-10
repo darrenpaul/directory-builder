c28930c97a805a0dd24e7db463c6358df5bd9d16
Darren <drobertpaul@gmail.com>
2025-01-10 07:52:53 +0200

    Add real stripe details

--- app/constants/stripe-price-id.ts
+++ app/constants/stripe-price-id.ts
@@ -1,3 +1,4 @@
 export enum StripePriceId {
-	SUBSCRIPTION_PRICE_ID = 'price_1Qf2vf4Z6SGvcX8vrJbhdnEa',
+	SUBSCRIPTION_PRICE_ID = 'price_1Qfb5oG2H6juA3rkloaQIvm4',
+	// SUBSCRIPTION_PRICE_ID = 'price_1Qf2vf4Z6SGvcX8vrJbhdnEa',
 }
