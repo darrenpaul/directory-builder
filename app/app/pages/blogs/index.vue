708733a397901c7f7faa94f1af93feb847dd25c7
Darren <drobertpaul@gmail.com>
2025-01-24 14:21:29 +0200

    Added metadata for blogs

--- app/app/pages/blogs/index.vue
+++ app/app/pages/blogs/index.vue
@@ -1,13 +1,67 @@
 <script setup lang="ts">
 import type { Blog } from '~~/types/blog'
-import { blogsApiRoute } from '~~/constants/routes-api'
+import { kebabCase, startCase } from 'lodash-es'
+import { blogsApiRoute, pageMetaApiRoute } from '~~/constants/routes-api'
+import UrlQueryBuilder from '~/lib/builders/url-query-builder'
 
 defineOgImageComponent('NuxtSeo')
 
-const { data } = await useFetch<{ data: Blog[], count: number }>(
-	blogsApiRoute.path,
-	{ method: 'GET' },
+const route = useRoute()
+
+const pageMetaUrlQueryBuilder = new UrlQueryBuilder(pageMetaApiRoute.path)
+
+const {
+	public: { siteUrl },
+} = useRuntimeConfig()
+
+const fetchPromises = []
+
+fetchPromises.push(
+	useFetch<{ data: Blog[], count: number }>(
+		blogsApiRoute.path,
+		{ method: 'GET' },
+	),
 )
+
+fetchPromises.push(
+	useFetch<PageMeta>(
+		pageMetaUrlQueryBuilder.withSlug({ slug: kebabCase(route.path as string) }).build(),
+		{ method: 'GET' },
+	),
+)
+
+const [{ data, error }, { data: pageMetaData }]
+  = await Promise.all(fetchPromises)
+
+if (error.value) {
+	throw createError({
+		statusCode: 500,
+		statusMessage: error.value?.message,
+	})
+}
+
+useHead({
+	link: [
+		{
+			hid: 'canonical',
+			rel: 'canonical',
+			href: `${siteUrl}${route.path}`,
+		},
+	],
+})
+
+useSeoMeta({
+	title: pageMetaData.value?.title || 'Nearby Nearby',
+	ogTitle: pageMetaData.value?.title || 'Nearby Nearby',
+	description: pageMetaData.value?.description || 'Nearby Nearby',
+	ogDescription: pageMetaData.value?.description || 'Nearby Nearby',
+	twitterCard: 'summary_large_image',
+})
+
+defineWebPage({
+	'@type': 'WebPage',
+	'image': pageMetaData.value?.image || '',
+})
 </script>
 
 <template>
